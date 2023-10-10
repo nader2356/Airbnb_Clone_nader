bdb27b052d525cd0da53cca6b6c390ced7d0fcfe
nader2356 <naderabdessaied8@gmail.com>
2023-10-10 23:09:08 +0200

    add navbar RegisterModal LoginModal in layout.tsx

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/navigation";
 
 import useLoginModal from "@/app/hooks/useLoginModal";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
-import useRentModal from "@/app/hooks/useRentModal";
+
 import { SafeUser } from "@/app/types";
 
 import MenuItem from "./MenuItem";
@@ -24,7 +24,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
 
   const loginModal = useLoginModal();
   const registerModal = useRegisterModal();
-  const rentModal = useRentModal();
+
 
   const [isOpen, setIsOpen] = useState(false);
 
@@ -37,8 +37,8 @@ const UserMenu: React.FC<UserMenuProps> = ({
       return loginModal.onOpen();
     }
 
-    rentModal.onOpen();
-  }, [loginModal, rentModal, currentUser]);
+    
+  }, [loginModal, currentUser]);
 
   return ( 
     <div className="relative">
@@ -118,10 +118,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                   label="My properties" 
                   onClick={() => router.push('/properties')}
                 />
-                <MenuItem 
-                  label="Airbnb your home" 
-                  onClick={rentModal.onOpen}
-                />
+                
                 <hr />
                 <MenuItem 
                   label="Logout" 
